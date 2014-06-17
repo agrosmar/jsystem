bb634ebb274ef4c42cc386d9e49c50fff447fdab
itaiag <itai.agmon@gmail.com>
2014-06-17 19:59:58 +0300

    Tests with failures are painted as red - Contributed by Erez Levy

--- jsystem-core-projects/jsystemCore/src/main/resources/jsystem/extensions/report/jsonToHtml/site/controllers/treeController.js
+++ jsystem-core-projects/jsystemCore/src/main/resources/jsystem/extensions/report/jsonToHtml/site/controllers/treeController.js
@@ -9,11 +9,11 @@ function populateChildren(source, destination) {
         switch (this.type) {
             case "scenario" :
                 var children = new Array();
-                destination.push({'text': this.name, 'icon': suiteIcon(this.status), 'children': children, 'type': this.status});
+                destination.push({'text': this.name, 'icon': suiteIcon(this.status), 'children': children, 'type': this.status , 'a_attr':{'class' : this.status}});
                 populateChildren(this.children, children);
                 break;
             case "test":
-                destination.push({'text': this.index + ". " + this.name, 'icon': testIcon(this.status), 'rel': this.status, 'type': this.status, 'a_attr': {'href': "tests/test_" + this.index+"/test.html"}});
+                destination.push({'text': this.index + ". " + this.name, 'icon': testIcon(this.status), 'rel': this.status, 'type': this.status, 'a_attr': {'href': "tests/test_" + this.index+"/test.html",'class':this.status}});
                 break;
         }
     });
